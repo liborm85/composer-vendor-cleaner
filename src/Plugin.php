d049994e5610195903daf0bc25574ef031be9b91
Libor M <liborm85@gmail.com>
2019-10-26 11:41:51 +0200

    init

---
+++ src/Plugin.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Liborm85\ComposerCleaner;
+
+use Composer\Composer;
+use Composer\EventDispatcher\EventSubscriberInterface;
+use Composer\IO\IOInterface;
+use Composer\Plugin\PluginInterface;
+
+class Plugin implements PluginInterface, EventSubscriberInterface
+{
+
+    /**
+     * @inheritDoc
+     */
+    public function activate(Composer $composer, IOInterface $io)
+    {
+        // TODO: Implement activate() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public static function getSubscribedEvents()
+    {
+        // TODO: Implement getSubscribedEvents() method.
+    }
+}
