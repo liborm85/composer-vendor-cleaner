3caf60f3b1b9bbb0c64d0be24e4421b1b4247d71
Libor M <liborm85@gmail.com>
2019-10-26 13:32:24 +0200

    plugin startup implementation

--- src/Plugin.php
+++ src/Plugin.php
@@ -3,22 +3,37 @@
 namespace Liborm85\ComposerVendorCleaner;
 
 use Composer\Composer;
+use Composer\Config;
 use Composer\EventDispatcher\EventSubscriberInterface;
 use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
 use Composer\Script\Event;
 use Composer\Script\ScriptEvents;
+use Composer\Util\Filesystem;
 
 class Plugin implements PluginInterface, EventSubscriberInterface
 {
+    const EXTRA_KEY = 'dev-files';
 
     /**
-     * @inheritDoc
+     * @var Composer
      */
-    public function activate(Composer $composer, IOInterface $io)
-    {
-        // TODO: Implement activate() method.
-    }
+    private $composer;
+
+    /**
+     * @var IOInterface
+     */
+    private $io;
+
+    /**
+     * @var Config
+     */
+    private $config;
+
+    /**
+     * @var Filesystem
+     */
+    private $filesystem;
 
     /**
      * @inheritDoc
@@ -30,8 +45,29 @@ class Plugin implements PluginInterface, EventSubscriberInterface
         ];
     }
 
+    /**
+     * @inheritDoc
+     */
+    public function activate(Composer $composer, IOInterface $io)
+    {
+        $this->composer = $composer;
+        $this->io = $io;
+        $this->config = $composer->getConfig();
+        $this->filesystem = new Filesystem();
+    }
+
     public function cleanup(Event $event)
     {
-        var_dump('Not implemented '.$event->getName().' callback');
+        $package = $this->composer->getPackage();
+        $extra = $package->getExtra();
+        $devFiles = isset($extra[self::EXTRA_KEY]) ? $extra[self::EXTRA_KEY] : null;
+        if (!$devFiles) {
+            return;
+        }
+
+        $vendorDir = $this->config->get('vendor-dir');
+
+        var_dump('Not implemented ' . $event->getName() . ' callback');
     }
+
 }
